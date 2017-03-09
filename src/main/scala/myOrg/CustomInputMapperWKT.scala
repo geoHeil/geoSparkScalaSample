4b8e76760cb171e748584b3b0cf3c8e89b2b6185
geoHeil <georg.heiler@me.com>
2017-03-09 13:47:50 +0100

    fix testcase

--- src/main/scala/myOrg/CustomInputMapperWKT.scala
+++ src/main/scala/myOrg/CustomInputMapperWKT.scala
@@ -19,34 +19,34 @@ class CustomInputMapperWKT extends FlatMapFunction[String, Polygon] {
     val lines = line.split(";")
     if (lines.head.startsWith("WKT")) {
       // skipping the header and returning empty iterator
-      logger.debug(s"skipping lines ${lines.head}")
+      logger.debug(s"skipping lines $line")
       Collections.emptyIterator()
     } else {
-      logger.debug(s"full lines ${line}")
-      val lineString = lines.head
-      logger.debug(lineString)
+      val geometryString = lines.head
+      val userData = lines.last.toInt // in my case user data is a single integer
+      logger.debug(s"Geometry of $geometryString with user data of $userData")
       try {
-        new WKTReader().read(lineString) match {
+        new WKTReader().read(geometryString) match {
           case m: MultiPolygon => {
+            logger.debug("using multipolygon")
             // as a java iterator must be returned it will be best to start using java collections from the start
             val result = new util.ArrayList[Polygon](m.getNumGeometries)
             var i = 0
             while (i < m.getNumGeometries) {
               val intermediateGeoObjectPolygon = m.getGeometryN(i).asInstanceOf[Polygon]
-              intermediateGeoObjectPolygon.setUserData(lineString.tail)
+              intermediateGeoObjectPolygon.setUserData(userData)
               // TODO figure out if this is still correct for MULTIPOLYGONs user data
               // will probably be required to be loaded separately for each polygon
-              logger.warn(s"using multy polygon, probably with wrong user data section of ${lineString.tail}")
+              logger.warn(s"using multipolygon, probably with wrong user data section of $userData")
               result.add(intermediateGeoObjectPolygon)
               i += 1
             }
             result.iterator
           }
           case p: Polygon => {
-            logger.warn(lineString.head)
-            logger.warn(lineString.tail)
-            logger.warn(s"using user data of ${lineString.tail}")
-            p.setUserData(lineString.tail)
+            logger.debug("using single polygon")
+            logger.debug(s"using user data of $userData")
+            p.setUserData(userData)
             Collections.singleton(p).iterator
           }
         }
--- src/test/scala/myOrg/CustomInputMapperWKTTest.scala
+++ src/test/scala/myOrg/CustomInputMapperWKTTest.scala
@@ -4,27 +4,23 @@ package myOrg
 
 import java.io.File
 
-import com.holdenkarau.spark.testing.{ DatasetSuiteBase, SharedSparkContext }
+import com.holdenkarau.spark.testing.{DatasetSuiteBase, SharedSparkContext}
 import com.vividsolutions.jts.geom.Polygon
 import org.apache.spark.storage.StorageLevel
 import org.datasyslab.geospark.spatialRDD.PolygonRDD
-import org.scalatest.{ FunSuite, Matchers }
+import org.scalatest.{FunSuite, Matchers}
 
+// TODO test spark unrelated parsing functionality maybe in a simple unit test without spark context.
+// anyhow seing this full integration work is nice too.
 class CustomInputMapperWKTTest extends FunSuite with SharedSparkContext with DatasetSuiteBase with Matchers {
-
   test("Check custom WKT format is read correctly") {
-
     val poly = new PolygonRDD(spark.sparkContext, "src"
       + File.separator + "main"
       + File.separator + "resources"
       + File.separator + "minimal01.csv", new CustomInputMapperWKT(), StorageLevel.MEMORY_ONLY)
 
     assert(poly.rawSpatialRDD.count == 3)
-    println(poly.rawSpatialRDD.first)
-    println("####################################### user data")
-    println(poly.rawSpatialRDD.first.asInstanceOf[Polygon].getUserData)
-    println("###################################### user data end")
-    assert(poly.rawSpatialRDD.first.asInstanceOf[Polygon].getUserData.toString.toInt == -120)
+    val userData: Int = poly.rawSpatialRDD.first.asInstanceOf[Polygon].getUserData.asInstanceOf[Int]
+    assert(userData == -120)
   }
-
 }
